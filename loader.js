bc08e66be2b469f87a8edfca8468d36e162c4045
Aaron Meier <aamea72@student.oaisd.org>
2025-03-17 09:54:10 -0400

    Yes

--- loader.js
+++ loader.js
@@ -1,12 +1,24 @@
-fetch("https://raw.githubusercontent.com/Aaron-Meier-Student/QRedesign/refs/heads/main/body.txt")
+fetch(
+    "https://raw.githubusercontent.com/Aaron-Meier-Student/QRedesign/refs/heads/main/body.txt"
+)
     .then((response) => response.text())
     .then((data) => {
         document.body.innerHTML = data;
     })
     .catch((error) => console.error("Error fetching text:", error));
-fetch("https://raw.githubusercontent.com/Aaron-Meier-Student/QRedesign/refs/heads/main/head.txt")
+fetch(
+    "https://raw.githubusercontent.com/Aaron-Meier-Student/QRedesign/refs/heads/main/head.txt"
+)
     .then((response) => response.text())
     .then((data) => {
         document.head.innerHTML = data;
+    })
+    .catch((error) => console.error("Error fetching text:", error));
+fetch(
+    "https://raw.githubusercontent.com/Aaron-Meier-Student/QRedesign/refs/heads/main/script.txt"
+)
+    .then((response) => response.text())
+    .then((data) => {
+        eval(data);
     })
     .catch((error) => console.error("Error fetching text:", error));
